febc89160f6193f67c4bc65b7559a7ca62c6634e
Julio Reyes <juliojesusreyes@gmail.com>
2023-06-16 16:22:51 -0400

    Exportando formato para render.

---
+++ index.js
@@ -0,0 +1,9 @@
+const jsonServer = require ("json-server"); // IMPORTANDO LIBREARIA JSON-SERVER
+const server = jsonServer.create ();
+const router = jsonServer.router ("db.json");
+const middlewares = jsonServer.defaults ();
+const port = process.env.PORT || 3001; // Aca puedes usar cualquier numero de puerto. Elegimos usar el puerto 3001 para evitar posibles conflictos.
+
+server.use (middlewares);
+server.use (router);
+server.listen (port);
